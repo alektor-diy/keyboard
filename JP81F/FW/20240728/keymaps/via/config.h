379f5f0984d0e464878927b6602bc88b19286491
アレくとーる <alektordiy@gmail.com>
2024-08-05 11:35:29 +0900

    Add files via upload

---
+++ JP81F/FW/20240728/keymaps/via/config.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#define VENDOR_ID       0x4144
+#define PRODUCT_ID      0x0001
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    alektor_diy
+#define PRODUCT         JP81F
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
